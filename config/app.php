e960cace7339248a310435415baed30dad6f8d5c
Esben Petersen <esbenspetersen@gmail.com>
2015-06-22 12:32:16 +0200

    Added cipher entry

--- config/app.php
+++ config/app.php
@@ -2,5 +2,6 @@
 
 return [
     'url' => 'http://oauth-tutorial.dev',
-    'key' => 'U<CdJu~T&.g/kR-NX55h]HfB+bb,b7Y*'
+    'key' => 'U<CdJu~T&.g/kR-NX55h]HfB+bb,b7Y*',
+    'cipher' => 'AES-256-CBC'
 ];
